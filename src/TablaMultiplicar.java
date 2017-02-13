90468c0a014e63c37696f07dfa5ce60faae48aaf
CIFPVILLA <CIFPVILLA@CIFPVILLA-PC>
2017-02-13 13:43:36 +0000

    segundo commit

--- src/TablaMultiplicar.java
+++ src/TablaMultiplicar.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class TablaMultiplicar {
 
 	public static void main(String[] args){
-		Scanner tec = new Scanner(System.in);
+		
+		//variable
 		int n;
+		double d;
+		
+		Scanner tec = new Scanner(System.in);
+		
+		//Introduciremos un valor para realizar la multiplicación
 		System.out.println("Introduce un número: ");
 		n = tec.nextInt();
+		
+		//Crearemos la tabla y calculamos dependiendo del valor que se le introduzca
 		System.out.println("tabla del "+n);
 		for(int i = 1; i <= 10; i++){
 			System.out.println(n+ " * "+i+ " = " +n*i);
